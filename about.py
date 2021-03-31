f9774de01bbf407878cb7637ade8f7e0f323b3d4
Alex Carrega <contact@alexcarrega.com>
2021-03-31 11:58:16 +0200

    Minor changes

--- about.py
+++ about.py
@@ -1,4 +1,5 @@
 project = 'GUARD'
+name = 'lcp'
 title = 'Local Control Plane'
 description = """In each local agent, the control plane is responsible for programmability, i.e.,
                  changing the behaviour of the data plane at run-time."""
